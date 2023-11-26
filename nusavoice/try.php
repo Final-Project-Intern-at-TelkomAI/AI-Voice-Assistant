77f2f978982a49247ffedd298b7d257deaec3a1f
jovidialaviosa <120235533+jovidialaviosa@users.noreply.github.com>
2023-11-26 12:34:44 +0700

    try.php

--- nusavoice/try.php
+++ nusavoice/try.php
@@ -3,57 +3,91 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Audio Upload</title>
+    <title>Streaming Audio Record and Upload</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 </head>
 
 <body>
-    <input type="file" id="audioFileInput">
-    <button id="uploadButton">Upload Audio</button>
+    <button id="recordButton">Start Recording</button>
 
     <div id="responseDisplay">
         <p id="textInput"></p>
         <p id="textAnswer"></p>
         <audio id="audioPlayer" controls></audio>
+        <audio id="audioPlayer2" controls></audio>
     </div>
 
     <script>
-        document.getElementById('uploadButton').addEventListener('click', function() {
-            const audioFileInput = document.getElementById('audioFileInput');
-            const file = audioFileInput.files[0];
+        let mediaRecorder;
+        let recordedChunks = [];
 
-            if (!file) {
-                alert('No file selected.');
-                return;
+        document.getElementById('recordButton').addEventListener('click', toggleRecording);
+
+        function toggleRecording() {
+            if (!mediaRecorder || mediaRecorder.state === 'inactive') {
+                startRecording();
+                document.getElementById('recordButton').textContent = 'Stop Recording';
+            } else {
+                stopRecording();
+                document.getElementById('recordButton').textContent = 'Start Recording';
             }
+        }
 
-            const formData = new FormData();
-            formData.append('audio', file, 'audio.wav');
+        function startRecording() {
+            navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(function (stream) {
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.ondataavailable = function (e) {
+                recordedChunks.push(e.data);
+            };
+            mediaRecorder.onstop = function () {
+                const audioBlob = new Blob(recordedChunks, { type: 'audio/wav' });
+                const formData = new FormData();
+                formData.append('audio', audioBlob, 'audio.wav');
 
-            $.ajax({
-                url: 'http://127.0.0.1:5000/askNusa',
-                type: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false,
-                processData: false,
-                headers: {
-                    'Access-Control-Allow-Origin': '*' // Change * to your specific origin if possible
-                },
-                            success: function(response) {
-                    const audioURL = "http://localhost/AI-Voice-Assistant/temp/audio_answer.wav";
-
+                fetch('http://127.0.0.1:5000/askNusa', {
+                    method: 'POST',
+                    body: formData,
+                    headers: {
+                        'Access-Control-Allow-Origin': '*' 
+                    },
+                })
+                .then(response => {
                     const audioElement = document.getElementById('audioPlayer');
-                    audioElement.src = audioURL;
+                    audioElement.src = URL.createObjectURL(audioBlob);
                     audioElement.controls = true;
                     audioElement.play();
-                },
-                error: function(error) {
-                    console.error('There was a problem with the AJAX request:', error);
-                }
-            });
+
+                    audioElement.addEventListener('ended', () => {
+                    const additionalAudioElement = document.getElementById('audioPlayer2');
+                    const audioURL = "http://localhost/AI-Voice-Assistant/temp/audio_answer.wav";
+                    additionalAudioElement.src = audioURL;
+                    additionalAudioElement.controls = true;
+                    additionalAudioElement.play();
+
+                    const responseDisplay = document.getElementById('responseDisplay');
+                    responseDisplay.appendChild(additionalAudioElement);
+                    });
+
+                    audioElement.play(); 
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch request:', error);
+                });
+            };
+            recordedChunks = [];
+            mediaRecorder.start();
+        })
+        .catch(function (err) {
+            console.error('Error accessing the microphone:', err);
         });
+}
+
+        function stopRecording() {
+            if (mediaRecorder.state !== 'inactive') {
+                mediaRecorder.stop();
+            }
+        }
     </script>
 </body>
 
